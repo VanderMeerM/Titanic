cc7f4bc0eeac89d78830960695e87609155cad7e
Marcel van der Meer <marcelmeer@gmail.com>
2020-06-12 18:27:23 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+
+
+   document.getElementById("vessel")
+   .addEventListener('click', checkScreenwidth);
+
+        function checkScreenwidth() {
+        if(window.outerWidth < 1350) {
+            document.querySelector('.moon').style.display="none";
+            document.querySelector('.darkmoon').style.display="none";
+            alert(           
+            'Unfortunately this animation is optimized for a minimum outer width of 1350 pixels.');   
+           }
+        else {move()};
+        } 
+   
+        function move() {
+            
+        
+         vessel.style.animation = "moving_sinking 20s";
+         
+         document.getElementById('iceberg').play(); 
+         
+         document.getElementById('time').innerHTML='April 14, 1912 23:40'; 
+         
+         setTimeout(() => {
+         document.getElementById('collission').play();
+          }, 2500); 
+
+          setTimeout(() => {
+            document.getElementById("heart").play();
+        },11000);
+
+         setTimeout(() => {
+             document.getElementById('time').innerHTML='April 15, 1912 0:30';
+        },13000);
+
+         setTimeout(() => {
+             document.getElementById('time').innerHTML='April 15, 1912 1:30';
+         }, 15000);
+            
+         setTimeout(() => {
+            document.getElementById('time').innerHTML='April 15, 1912 2:00';
+        }, 17000);
+
+         setTimeout(() => {
+             document.getElementById('time').innerHTML='April 15, 1912 2:20'
+        },19000);
+
+        setTimeout(() => {
+            document.getElementById('time').innerHTML='Reload this page for another experience'
+       },22000);
+        
+       }
+               
+                   
+
+            
+        
+             
