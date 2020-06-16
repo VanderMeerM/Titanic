feb1af48fedc7eb5fa2e765e6fdf02b91d97ad80
Marcel van der Meer <marcelmeer@gmail.com>
2020-06-16 16:24:21 +0200

    some approvements

--- script.js
+++ script.js
@@ -1,59 +1,59 @@
 
 
-   document.getElementById("vessel")
-   .addEventListener('click', checkScreenwidth);
+document.getElementById("vessel")
+    .addEventListener('click', checkScreenwidth);
 
-        function checkScreenwidth() {
-        if(window.outerWidth < 1350) {
-            document.querySelector('.moon').style.display="none";
-            document.querySelector('.darkmoon').style.display="none";
-            alert(           
-            'Unfortunately this animation is optimized for a minimum outer width of 1350 pixels.');   
-           }
-        else {move()};
-        } 
-   
-        function move() {
-            
-        
-         vessel.style.animation = "moving_sinking 20s";
-         
-         document.getElementById('iceberg').play(); 
-         
-         document.getElementById('time').innerHTML='April 14, 1912 23:40'; 
-         
-         setTimeout(() => {
-         document.getElementById('collission').play();
-          }, 2500); 
+function checkScreenwidth() {
+    if (window.outerWidth < 1350) {
+        document.querySelector('.moon').style.display = "none";
+        document.querySelector('.darkmoon').style.display = "none";
+        alert(
+            'Unfortunately this animation is optimized for a minimum (outer) width of 1350 pixels.');
+    }
+    else { move() };
+}
 
-          setTimeout(() => {
-            document.getElementById("heart").play();
-        },11000);
+function move() {
 
-         setTimeout(() => {
-             document.getElementById('time').innerHTML='April 15, 1912 0:30';
-        },13000);
+    vessel.style.animation = "moving_sinking 20s";
 
-         setTimeout(() => {
-             document.getElementById('time').innerHTML='April 15, 1912 1:30';
-         }, 15000);
-            
-         setTimeout(() => {
-            document.getElementById('time').innerHTML='April 15, 1912 2:00';
-        }, 17000);
+    document.getElementById('iceberg').play();
 
-         setTimeout(() => {
-             document.getElementById('time').innerHTML='April 15, 1912 2:20'
-        },19000);
+    document.getElementById('time').innerHTML = 'April 14, 1912 23:40';
 
-        setTimeout(() => {
-            document.getElementById('time').innerHTML='Reload this page for another experience'
-       },22000);
-        
-       }
-               
-                   
+    setTimeout(() => {
+        document.getElementById('collission').play();
+    }, 2500);
 
-            
-        
-             
+    setTimeout(() => {
+        document.getElementById("heart").play();
+    }, 11000);
+
+    setTimeout(() => {
+        document.getElementById('time').innerHTML = 'April 15, 1912 0:30';
+    }, 13000);
+
+    setTimeout(() => {
+        document.getElementById('time').innerHTML = 'April 15, 1912 1:30';
+    }, 15000);
+
+    setTimeout(() => {
+        document.getElementById('time').innerHTML = 'April 15, 1912 2:00';
+    }, 17000);
+
+    setTimeout(() => {
+        document.getElementById('time').innerHTML = 'April 15, 1912 2:20';
+        document.getElementById('origin').style.visibility = "hidden";
+        document.getElementById('wreck').style.visibility = "visible";
+    }, 19000);
+
+    setTimeout(() => {
+        document.getElementById('time').innerHTML = 'Reload this page for another experience'
+    }, 22000);
+
+}
+
+
+
+
+
